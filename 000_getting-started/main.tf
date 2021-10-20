b4cfda8c51536e8a1bf258c17a6f1b52b18be3a4
serguy <devops@harpy-tech.com>
2021-10-20 20:07:35 +0100

    add ec2-instance tag

--- 000_getting-started/main.tf
+++ 000_getting-started/main.tf
@@ -1,4 +1,14 @@
+provider "aws" {
+  # Configuration options
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "default"
+  region                  = "us-east-1"
+}
 resource "aws_instance" "app_server" {
   ami           = "ami-0416f96ae3d1a3f29"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "name-ec2-instance"
+  }
 }
